4e0f7484fa27e5e686e36a959ab7d94e4a1e70cb
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-06-22 13:44:56 -0400

    init the project

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,3 +1,4 @@
+import Entities.AcademyGroup;
 import Repositoty.AcademyGroupRepository;
 
 public class main {
@@ -5,6 +6,13 @@ public class main {
     public static void main(String[] args) {
         AcademyGroupRepository repo = new AcademyGroupRepository("group.file");
         repo.LoadFromFile();
-        repo.Add(new);
+        repo.Add(new AcademyGroup(1, "GrowthHungry batch 25", "")); //in the memory of the app at the moment
+        repo.SaveChanges(); //it serialized and saved to the file "group.file"
+
+        //next time someone loads the file and can see whats the content
+        repo.LoadFromFile();
+        for (AcademyGroup academyGroup : repo.GetAll()) {
+            System.out.println(academyGroup.getName());
+        }
     }
 }
