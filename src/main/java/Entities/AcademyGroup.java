460f333f52a635fd5aa8aa359920a785ffb915ad
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-06-22 10:11:33 -0400

    init the project

---
+++ src/main/java/Entities/AcademyGroup.java
@@ -0,0 +1,33 @@
+package Entities;
+
+public class AcademyGroup {
+    private int id;
+    private String name;
+
+    //add sensitive field with transient
+    private transient String discordLink;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDiscordLink() {
+        return discordLink;
+    }
+
+    public void setDiscordLink(String discordLink) {
+        this.discordLink = discordLink;
+    }
+}
