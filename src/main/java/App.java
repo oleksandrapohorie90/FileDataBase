95a7eaf41509aba56a31849df5127e90c4f23349
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-06-25 19:51:55 -0400

    init the project

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,17 +9,17 @@ public class App {
     public static void main(String[] args) {
         DBContext dbContext = new DBContext("db.file");
         //created some data in memory
-//        Academy newAcademy = new Academy(1, "GrowthHungry");
-//        Group newGroup = new Group(1, "GrowthHungry batch 26", "discord.com");
-//        newGroup.setAcademyId(newAcademy.getId());
-//
-//        //lets save it to the file
-//        AcademyRepository academyRepository = new AcademyRepository(dbContext);
-//        academyRepository.Add(newAcademy);
-//        //academyRepository.SaveChanges();
-//
-//        GroupRepository groupRepository = new GroupRepository(dbContext);
-//        groupRepository.Add(newGroup);
+        Academy newAcademy = new Academy(1, "GrowthHungry");
+        Group newGroup = new Group(1, "GrowthHungry batch 26", "discord.com");
+        newGroup.setAcademyId(newAcademy.getId());
+
+        //lets save it to the file
+        AcademyRepository academyRepository = new AcademyRepository(dbContext);
+        academyRepository.Add(newAcademy);
+        //academyRepository.SaveChanges();
+
+        GroupRepository groupRepository = new GroupRepository(dbContext);
+        groupRepository.Add(newGroup);
 
         System.out.println(dbContext.GetDatabase().toString());
     }
